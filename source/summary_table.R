76bc39bf04f4591a0e101898a3e4eca7012d38d1
Max Stewart <mzs11@uw.edu>
2022-11-13 14:22:57 -0800

    simplified data file names

---
+++ source/summary_table.R
@@ -0,0 +1,3 @@
+source("../data/pregnancy_related_mortality_ratio_by_race_ethnicity__2016_2018.csv")
+source("../data/pregnancy_related_mortality_ratio_by_urban_rural_classifications__2016_2018.csv")
+source("../data/us_hospital_locations.csv")
