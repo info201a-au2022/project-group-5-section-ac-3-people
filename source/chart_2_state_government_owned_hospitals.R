356f739ea5a3d6800e567fdfffdc3cfa0f643f5c
rimintz@uw.edu <rimintz@uw.edu>
2022-11-14 15:40:02 -0800

    troubleshooting

--- source/chart_2_state_government_owned_hospitals.R
+++ source/chart_2_state_government_owned_hospitals.R
@@ -1,10 +1,11 @@
 library(ggplot2)
-world_shape <- map_data("world")
-view(world_shape)
-ggplot(world_shape) +
-  geom_polygon(
-    mapping = aes(x = long, y = lat, group = group),
-    color = "white",
-    size = .1
-  ) +
-  coord_map()
+library(tidyverse)
+# Making state/owned by state gov't data frame
+view(hospital_owners_by_state)
+state_gov_hospitals <- data.frame(hospital_owners_by_state$STATE, hospital_owners_by_state$owner_state_govt)
+view(state_gov_hospitals)
+state_gov_hospitals <- state_gov_hospitals %>% 
+  rename(region = state)
+state_gov_hospitals <- state_gov_hospitals %>% 
+  rename(owned_by_state_govt = hospital_owners_by_state.owner_state_govt)
+view(state_gov_hospitals)
